3ad59a14babd443f768f68fb0a52fc8879f0a07f
Bhavya Kumar <54398457+BhavyaKumar17@users.noreply.github.com>
2020-01-30 00:14:08 +0530

    updated

--- src/components/MenuRight.js
+++ src/components/MenuRight.js
@@ -15,27 +15,32 @@ class MenuRight extends React.Component {
         <div className="MenuRightMain">
             <h3 className="MenuRightHeading">Top Picks</h3>
                 <div className="grid-container">  
-                    {[...new Set(this.props.allItems.map(i=>i[1].category))].map(ch=><div key={ch}>
-                        {ch}
-                        {console.log(`category: ${ch}`)}
-                        {this.props.allItems.filter(item=>{
-                            return(item[1].category === ch? (<div key={item[0]} className="grid-item">
-                                <img className="dishImage" alt="Yumlane Pizza" 
-                                     src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${item[1].cloudinaryImageId}`} />
-                                <div key={item[0]} className="dishName" >
-                                    {console.log(item[1].name)} 
-                                    <span style={{paddingLeft: "2em"}}>
-                                        ₹{console.log(item[1].price)}
-                                    </span>
-                                </div>
-                                <div>
-                                    <button className="btn btn-light" onClick={() => this.props.addItemToCart(item[1].name)}>+</button>
-                                    {this.props.cart[item[1].name]}
-                                    <button className="btn btn-light" onClick={()=> this.props.removeItemFromCart(item[1].name)}>-</button>
-                                </div>
-                            </div>):console.log("out of loop"))
-                        })}
-                    </div>)}
+                {console.log(this.props.allItems)}
+                    {[...new Set(this.props.allItems.map(i=>i[1].category))].map(ch=>{
+                        return <div key={ch}>
+                            {ch}
+                            {console.log(`category: ${ch}`)}
+                            {this.props.allItems.filter(item=>{
+                                if(item[1].category === ch){ 
+                                    return( <div key={item[0]} className="grid-item">
+                                        <img className="dishImage" alt="Yumlane Pizza" 
+                                            src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${item[1].cloudinaryImageId}`} />
+                                        <div className="dishName" >
+                                            {item[1].name} 
+                                            <span style={{paddingLeft: "2em"}}>
+                                                ₹{item[1].price}
+                                            </span>
+                                        </div>
+                                        <div>
+                                            <button className="btn btn-light" onClick={() => this.props.addItemToCart(item[1].name)}>+</button>
+                                                {this.props.cart[item[1].name]}
+                                            <button className="btn btn-light" onClick={()=> this.props.removeItemFromCart(item[1].name)}>-</button>
+                                        </div>
+                                    </div>)
+                                
+                            }})}
+                        </div>
+                    })}
                 </div>
         </div>
     )}
